312f17040f3da79d9f3446639b3a3b3132d37089
dima_dencep <dima_dencep@redlance.org>
2024-06-14 14:46:56 +0700

    Neoforge (1.21)

--- forge/src/main/java/io/github/kosmx/bendylib/neoforge/ForgeModInterface.java
+++ forge/src/main/java/io/github/kosmx/bendylib/neoforge/ForgeModInterface.java
@@ -1,22 +1,17 @@
 package io.github.kosmx.bendylib.neoforge;
 
 import io.github.kosmx.bendylib.compat.tr7zw.TDSkinCompat;
+import net.neoforged.api.distmarker.Dist;
 import net.neoforged.fml.ModList;
 import net.neoforged.fml.common.Mod;
-import net.neoforged.fml.loading.FMLLoader;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Mod("bendylib")
+@Mod(value = "bendylib", dist = Dist.CLIENT)
 public class ForgeModInterface {
     public static Logger LOGGER = LoggerFactory.getLogger("bendy-lib");
 
     public ForgeModInterface() {
-        if (!FMLLoader.getDist().isClient()) {
-            LOGGER.warn("You're loading a client-only mod on server-side");
-            LOGGER.warn("Most likely it won't be a problem");
-        }
-
         if (ModList.get().isLoaded("skinlayers3d")) {
             LOGGER.info("Initializing 3D Skin Layers compatibility");
 
